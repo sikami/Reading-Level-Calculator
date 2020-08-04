bbb4732b1182d9099027430058dd4801d8a07576
sikami <widyasari.listya@gmail.com>
2020-08-04 16:06:07 +0100

    added constructor

---
+++ readability/Readability.java
@@ -0,0 +1,15 @@
+package readability;
+
+public class Readability {
+
+    //a string
+    //calculate letters, words, sentence
+
+    private String text;
+
+    public Readability(String text) {
+        this.text = text;
+    }
+
+
+}
